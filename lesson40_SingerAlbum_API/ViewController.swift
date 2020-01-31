0c9b076a8d679090e419fc7760a067a4120227cc
rakhmatillo <rakhmatillo.topiboldiev@gmail.com>
2020-01-31 11:22:42 +0500

    Initial Commit

---
+++ lesson40_SingerAlbum_API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson40_SingerAlbum_API
+//
+//  Created by Rakhmatillo Topiboldiev on 1/31/20.
+//  Copyright © 2020 Rakhmatillo Topiboldiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
